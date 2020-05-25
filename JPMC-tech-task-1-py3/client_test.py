f5624748bbe44c9477ac2849f7eb510cc9abdd5f
Jeet1912 <51080094+Jeet1912@users.noreply.github.com>
2020-05-25 21:36:21 +0530

    Add files via upload

---
+++ JPMC-tech-task-1-py3/client_test.py
@@ -0,0 +1,54 @@
+import unittest
+from client3 import getDataPoint
+from client3 import getRatio
+
+class ClientTest(unittest.TestCase):
+  def test_getDataPoint_calculatePrice(self):
+    quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+          self.assertEqual(getDataPoint(quote),(quote['stock'],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+quote['top_ask']['price'])/2))
+          
+
+  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+          self.assertEqual(getDataPoint(quote),(quote['stock'],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+quote['top_ask']['price'])/2))
+          
+
+
+  """ ------------ Add more unit tests ------------ """
+  def test_getRatio(self):
+    ratios = [
+      {'price_a' : 121.2, 'price_b': 120.48},
+      {'price_a': 121.68,'price_b': 117.87}
+    ]
+  
+    for ratio in ratios:
+          self.assertEqual(getRatio(ratio['price_a'],ratio['price_b']),((ratio['price_a']/ratio['price_b'])))
+  
+  def test_getRatio_nullA(self):
+    ratios = [
+      {'price_a' : 0, 'price_b': 121},
+      {'price_a': 0,'price_b': 117.87}
+    ]
+    for ratio in ratios:
+          self.assertEqual(getRatio(ratio['price_a'],ratio['price_b']),0)
+  
+  def test_getRatio_nullB(self):
+     self.assertRaises(ZeroDivisionError,getRatio,1,0)
+
+
+       
+      
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ JPMC-tech-task-3-py3/src/Graph.tsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Table } from '@jpmorganchase/perspective';
+import { ServerRespond } from './DataStreamer';
+import { DataManipulator } from './DataManipulator';
+import './Graph.css';
+
+interface IProps {
+  data: ServerRespond[],
+}
+
+interface PerspectiveViewerElement extends HTMLElement {
+  load: (table: Table) => void,
+}
+class Graph extends Component<IProps, {}> {
+  table: Table | undefined;
+
+  render() {
+    return React.createElement('perspective-viewer');
+  }
+
+  componentDidMount() {
+    // Get element from the DOM.
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+
+    // updating schema
+    const schema = {
+      price_abc: 'float',
+      price_def:'float',
+      ratio:'float',         // comparing ratios of two stocks
+      timestamp: 'date',
+      upper_bound:'float',
+      lower_bound:'float',
+      trigger_alert:'float', // point where the bounds are crossed
+    };
+
+
+    if (window.perspective && window.perspective.worker()) {
+      this.table = window.perspective.worker().table(schema);
+    }
+    if (this.table) {
+      // Load the `table` in the `<perspective-viewer>` DOM reference.
+      //reflecting the changes made in the schema & updating the table
+      elem.load(this.table);
+      elem.setAttribute('view', 'y_line');
+      elem.setAttribute('row-pivots', '["timestamp"]');
+      elem.setAttribute('columns', '["ratio","lower_bound","upper_bound","trigger_alert"]');
+      elem.setAttribute('aggregates', JSON.stringify({
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
+        timestamp: 'distinct count',
+        upper_bound:'avg',
+        lower_bound:'avg',
+        trigger_alert: 'avg', 
+      }));
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.table) {
+      this.table.update([
+        DataManipulator.generateRow(this.props.data),   //changing the argument
+      ]);         
+    }
+  }
+}
+
+export default Graph;
